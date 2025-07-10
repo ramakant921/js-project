3cdf309041ab65d10db4b46e4d90df254bfd7445
ramakant921 <ramakantm921@gmail.com>
2025-07-10 06:56:48 +0000

    operation

---
+++ .devcontainer/basics/operation.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log (valueInNumber);
+
+
+// "45"= 45
+// "45fvashie"=NaN
+// true=1
+// false=0
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// "" = false
+// "fewyhgrfygp" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
