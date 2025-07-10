3277e7129746d5dacd88cc2038e2b69f34c200ef
ramakant921 <ramakantm921@gmail.com>
2025-07-10 05:52:05 +0000

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+// code should be readable
+
+console.log("welcome")
+
+
+
+
+
+
+// number = 2 to the power 53
+// bigint
+// string is used in singe and double qoutes ""   '
+// boolean = true/false
+// null is a stand alone value
+// undefined = when value is not defined
+// symbol is used for uniqueness 
+
+
+// object
+console.log(typeof String)
