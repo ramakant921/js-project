8d318f9d3816cc01a3edc39b944db5317766fc90
ramakant921 <ramakantm921@gmail.com>
2025-07-11 14:33:41 +0000

    math and num

---
+++ .devcontainer/basics/6_mathsnum.js
@@ -0,0 +1,32 @@
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+const otheNumber = 23.456
+
+// console.log(otheNumber.toPrecision(10));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-UK'));
+
+
+// ++++++++++ MATHS ++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(42364734.632423746));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4.9));
+// console.log(Math.ceil(4.6));
+// console.log(Math.max(4,6,8,2));
+
+console.log(Math.random());
+console.log((Math.random()*10)) + 1;
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
