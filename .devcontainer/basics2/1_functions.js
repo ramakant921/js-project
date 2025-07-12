34dc153a5d879edfa105d9efd034936d0b22ebb9
ramakant921 <ramakantm921@gmail.com>
2025-07-12 18:30:59 +0000

    functions

---
+++ .devcontainer/basics2/1_functions.js
@@ -0,0 +1,17 @@
+
+function sayMyName(){
+    console.log("B");
+    console.log("A");
+    console.log("T");
+    console.log("M");
+    console.log("A");
+    console.log("N"); 
+}
+
+// sayMyName()
+
+function addTwoNumber(no1 , no2){
+    console.log(no1 + no2);   
+}
+
+addTwoNumber(15,5);
