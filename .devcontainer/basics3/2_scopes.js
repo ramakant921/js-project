0f213c58a4a559ccf61e212f502914e5d8c2c6d1
ramakant921 <ramakantm921@gmail.com>
2025-07-13 11:42:10 +0000

    scope

---
+++ .devcontainer/basics3/2_scopes.js
@@ -0,0 +1,31 @@
+// var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 25
+    // console.log("INNER:",a);
+}
+
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "spiderman"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+one()
+
