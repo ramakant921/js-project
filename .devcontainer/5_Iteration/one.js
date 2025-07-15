d11dfb5bbd85a221c775d68781f82e078d466804
ramakant921 <ramakantm921@gmail.com>
2025-07-15 18:10:22 +0000

    iteration

--- .devcontainer/5_Iteration/one.js
+++ .devcontainer/5_Iteration/one.js
@@ -1,8 +1,37 @@
 // for
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        // console.log("5 is the worst number")
+    }
+    // console.log(element);
+
+
+}
+
+// console.log(element);          // this is not defined outside the element 
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value ${i}`);
+   for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + '*' + j + '=' + i*j);
+    }
+    
+}
+let myArray = ["spiderman","thor","superman"]
+// console.log(myArray.length)
+for (let index = 0; index < myArray.length; index) {
+    const element = myArray[index];
+    // console.log(element);
     
 }
 
 
-console.log()
+//Break and Continue
+
+
+for (let index = 1; index <= 20; index++) {
+    console.log(`value of i is${i}`);
+    
+}
