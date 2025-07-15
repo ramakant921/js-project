25d344c029ae5d305e25b87b2f0f06c93b8d31e0
ramakant921 <ramakantm921@gmail.com>
2025-07-15 05:13:42 +0000

    iife

---
+++ .devcontainer/basics3/4_IIFE.js
@@ -0,0 +1,11 @@
+// immediately Invoked Function Expression (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('spiderman');
