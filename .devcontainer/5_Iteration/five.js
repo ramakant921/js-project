10aa50f7d05bfa717bdc2bfb92486aa81df8bf9e
ramakant921 <ramakantm921@gmail.com>
2025-07-16 17:28:57 +0000

    fives

--- .devcontainer/5_Iteration/five.js
+++ .devcontainer/5_Iteration/five.js
@@ -14,6 +14,26 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 
 // coding.forEach(printMe)
 
-coding.forEach( (item, index, arr)=>{
-    console.log(item, index, arr);
+// coding.forEach( (item, index, arr)=>{
+    // console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((item)=>{
+
+    console.log(item.languageName);
 })
